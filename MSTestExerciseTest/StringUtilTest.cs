d5dcc168d5a761738a92287287f64bc57f5930a6
nakaji <nakaji126@gmail.com>
2013-08-20 22:14:31 +0900

    18.1 「aaa」を入力すると、「aaa」を返す

---
+++ MSTestExerciseTest/StringUtilTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestExercise.Test
+{
+    [TestClass]
+    public class StringUtilTest
+    {
+        [TestMethod]
+        public void aaaを入力するとaaaが取得できる()
+        {
+            var result = StringUtil.ToSnakeCase("aaa");
+
+            Assert.AreEqual("aaa", result);
+        }
+    }
+}
