00183345425943fdaa2fdc4a311b94c629984099
nakaji <nakaji126@gmail.com>
2013-08-21 01:29:53 +0900

    18.3 状態に応じてネストさせる

--- MSTestExerciseTest/CounterTest.cs
+++ MSTestExerciseTest/CounterTest.cs
@@ -8,38 +8,67 @@ namespace MSTestExerciseTest
     [TestClass]
     public class CounterTest
     {
-        private Counter _sut;
-
-        [TestInitialize]
-        public void SetUp()
+        [TestClass]
+        private class 初期状態
         {
-            _sut = new Counter();
+            private Counter _sut;
+
+            [TestInitialize]
+            public void SetUp()
+            {
+                _sut = new Counter();
+            }
+
+            [TestMethod]
+            public void 初期状態では1が返る()
+            {
+                var actual = _sut.Increment();
+
+                Assert.AreEqual(1, actual);
+            }
         }
 
-        [TestMethod]
-        public void 初期状態では1が返る()
+        [TestClass]
+        private class 実行2回目
         {
-            var actual = _sut.Increment();
+            private Counter _sut;
 
-            Assert.AreEqual(1, actual);
-        }
-
-        [TestMethod]
-        public void 実行2回目は2が返る()
-        {
-            _sut.Increment();
-            var actual = _sut.Increment();
-
-            Assert.AreEqual(2, actual);
-        }
-        [TestMethod]
-        public void 実行51回目は51が返る()
-        {
-            Enumerable.Range(1, 50).ToList().ForEach(x => _sut.Increment());
+            [TestInitialize]
+            public void SetUp()
+            {
+                _sut = new Counter();
+                _sut.Increment();
+            }
             
-            var actual = _sut.Increment();
+            [TestMethod]
+            public void 実行2回目は2が返る()
+            {
+                var actual = _sut.Increment();
 
-            Assert.AreEqual(51, actual);
+                Assert.AreEqual(2, actual);
+            }
+        }
+
+        [TestClass]
+        private class 実行51回目
+        {
+            private Counter _sut;
+
+            [TestInitialize]
+            public void SetUp()
+            {
+                _sut = new Counter();
+                Enumerable.Range(1, 50).ToList().ForEach(x => _sut.Increment());
+            }
+
+            [TestMethod]
+            public void 実行51回目は51が返る()
+            {
+
+                var actual = _sut.Increment();
+
+                Assert.AreEqual(51, actual);
+            }
         }
     }
 }
