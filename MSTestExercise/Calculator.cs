e17a00d835dd69d73eb47107ba28b565dae75a78
nakaji <nakaji126@gmail.com>
2013-08-21 00:48:17 +0900

    18.2 8と4で2が返る

---
+++ MSTestExercise/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestExercise
+{
+    public static class Calculator
+    {
+        public static int Divide(int x, int y)
+        {
+            return 2;
+        }
+    }
+}
---
+++ MSTestExerciseTest/CalculatorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExercise;
+
+namespace MSTestExerciseTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void 割り算_8と4で2が返る()
+        {
+            var result = Calculator.Divide(8, 4);
+        }
+    }
+}
