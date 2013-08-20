e0f0999bebfe89c9521c734c4adfc195186d4994
nakaji <nakaji126@gmail.com>
2013-08-21 01:13:00 +0900

    18.3 初期状態では1が返る

---
+++ MSTestExercise/Counter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSTestExercise
+{
+    public class Counter
+    {
+        public object Increment()
+        {
+            return 1;
+        }
+    }
+}
---
+++ MSTestExerciseTest/CounterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExercise;
+
+namespace MSTestExerciseTest
+{
+    [TestClass]
+    public class CounterTest
+    {
+        [TestMethod]
+        public void 初期状態では1が返る()
+        {
+            var sut = new Counter();
+
+            var actual = sut.Increment();
+
+            Assert.AreEqual(1, actual);
+        }
+    }
+}
