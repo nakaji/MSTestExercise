e8433006335ddc09ed2d75a0bcdd8443d0aa41c5
nakaji <nakaji126@gmail.com>
2013-08-21 23:02:53 +0900

    18.4 2の時はtrue

---
+++ MSTestExercise/NumberUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestExercise
+{
+    public static class NumberUtil
+    {
+        public static bool IsEven(int num)
+        {
+            return true;
+        }
+    }
+}
---
+++ MSTestExerciseTest/NumberUtilTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExercise;
+
+namespace MSTestExerciseTest
+{
+    [TestClass]
+    public class NumberUtilTest
+    {
+        [TestMethod]
+        public void 引数が2の時はtrue()
+        {
+            Assert.IsTrue(NumberUtil.IsEven(2));
+        }
+    }
+}
