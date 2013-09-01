6b19fa0236e6e7016aacb625984efb30791bf0a3
nakaji <nakaji126@gmail.com>
2013-09-01 23:49:52 +0900

    18.6 コンストラクタでRunnableオブジェクトを引数に持つ

---
+++ MSTestExercise/BackgroundTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSTestExercise
+{
+    public class BackgroundTask
+    {
+        public BackgroundTask(Runnable runnable)
+        {
+            
+        }
+    }
+
+    public class Runnable
+    {
+    }
+}
---
+++ MSTestExerciseTest/BackgroundTaskTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExercise;
+
+namespace MSTestExerciseTest
+{
+    [TestClass]
+    public class BackgroundTaskTest
+    {
+        [TestClass]
+        public class オブジェクト生成
+        {
+            [TestMethod]
+            public void コンストラクタでRunnableオブジェクトを引数に持つ()
+            {
+                var sut = new BackgroundTask(new Runnable());
+            }
+        }
+    }
+}
