8a4dc4918d7f254bc7397f827b2968241f62c12f
nakaji <nakaji126@gmail.com>
2013-09-02 23:49:35 +0900

    19.1 refactoring

--- MSTestExerciseTest/chap19/FizzBuzzTest.cs
+++ MSTestExerciseTest/chap19/FizzBuzzTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MSTestExercise.chap19;
 
@@ -11,7 +12,8 @@ namespace MSTestExerciseTest.chap19
         [TestMethod]
         public void 引数が1の時()
         {
-            var result = FizzBuzz.CreateFizzBuzzList(1);
+            var num = 1;
+            var result = FizzBuzz.CreateFizzBuzzList(num);
 
             var expected = new List<String>() { "1" };
 
@@ -21,38 +23,43 @@ namespace MSTestExerciseTest.chap19
         [TestMethod]
         public void 引数が2の時()
         {
-            var result = FizzBuzz.CreateFizzBuzzList(2);
+            var num = 2;
+            var result = FizzBuzz.CreateFizzBuzzList(num);
 
             var expected = new List<String>() { "1", "2" };
 
-            Assert.AreEqual(expected[0], result[0]);
-            Assert.AreEqual(expected[1], result[1]);
+            foreach (var i in Enumerable.Range(0, num))
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
         }
 
         [TestMethod]
         public void 引数が3の時()
         {
-            var result = FizzBuzz.CreateFizzBuzzList(3);
+            var num = 3;
+            var result = FizzBuzz.CreateFizzBuzzList(num);
 
             var expected = new List<String>() { "1", "2", "Fizz" };
 
-            Assert.AreEqual(expected[0], result[0]);
-            Assert.AreEqual(expected[1], result[1]);
-            Assert.AreEqual(expected[2], result[2]);
+            foreach (var i in Enumerable.Range(0, num))
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
         }
 
         [TestMethod]
         public void 引数が5の時()
         {
-            var result = FizzBuzz.CreateFizzBuzzList(5);
+            var num = 5;
+            var result = FizzBuzz.CreateFizzBuzzList(num);
 
             var expected = new List<String>() { "1", "2", "Fizz", "4", "Buzz" };
 
-            Assert.AreEqual(expected[0], result[0]);
-            Assert.AreEqual(expected[1], result[1]);
-            Assert.AreEqual(expected[2], result[2]);
-            Assert.AreEqual(expected[3], result[3]);
-            Assert.AreEqual(expected[4], result[4]);
+            foreach (var i in Enumerable.Range(0, num))
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
         }
     }
 }
