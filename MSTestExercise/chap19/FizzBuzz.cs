daa070f85b6648e02b6ce08380a2987f27261318
nakaji <nakaji126@gmail.com>
2013-09-02 23:03:54 +0900

    19.1 引数が1の時

---
+++ MSTestExercise/chap19/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestExercise.chap19
+{
+    public class FizzBuzz
+    {
+        public static List<String> CreateFizzBuzzList(int p)
+        {
+            return new List<String>() {"1"};
+        }
+    }
+}
---
+++ MSTestExerciseTest/chap19/FizzBuzzTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExercise.chap19;
+
+namespace MSTestExerciseTest.chap19
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void 引数が1の時()
+        {
+            var result = FizzBuzz.CreateFizzBuzzList(1);
+            var expected = new List<String>() {"1"};
+            Assert.AreSame(expected[0], result[0]);
+        }
+    }
+}
