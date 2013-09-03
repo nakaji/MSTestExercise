7b721e71dcfe88ba45dd83dfc91c18a853fcad20
nakaji <nakaji126@gmail.com>
2013-09-04 01:50:39 +0900

    19.2 Employeeクラスを追加

---
+++ MSTestExercise/chap19/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSTestExercise.chap19
+{
+    public class Employee
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+
+        public Employee(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
---
+++ MSTestExerciseTest/chap19/EmployeeTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExercise;
+using MSTestExercise.chap19;
+
+namespace MSTestExerciseTest.chap19
+{
+    [TestClass]
+    public class EmployeeTest
+    {
+        [TestMethod]
+        public void Employeeオブジェクトの生成()
+        {
+            var sut = new Employee("FirstName", "LastName", "Email");
+
+            Assert.AreEqual("FirstName", sut.FirstName);
+            Assert.AreEqual("LastName", sut.LastName);
+            Assert.AreEqual("Email", sut.Email);
+
+        }
+    }
+}
