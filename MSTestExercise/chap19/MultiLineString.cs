47512b95ce2a9dbaf8410988fd884636e437e250
nakaji <nakaji126@gmail.com>
2013-09-04 22:50:55 +0900

    19.3 引数の文字列を改行で連結する

---
+++ MSTestExercise/chap19/MultiLineString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestExercise.chap19
+{
+    public static class MultiLineString
+    {
+        public static string Join(params string[] args)
+        {
+            var result = args.First();
+            foreach (var str in args.Skip(1))
+            {
+                result += "\n" + str;
+            }
+            return result;
+        }
+    }
+}
---
+++ MSTestExerciseTest/chap19/MultiLineStringTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExercise.chap19;
+
+namespace MSTestExerciseTest.chap19
+{
+    [TestClass]
+    public class MultiLineStringTest
+    {
+        [TestMethod]
+        public void 引数の文字列を改行で連結する()
+        {
+            var result = MultiLineString.Join("Hello", "World");
+
+            Assert.AreEqual("Hello\nWorld", result);
+        }
+    }
+}
