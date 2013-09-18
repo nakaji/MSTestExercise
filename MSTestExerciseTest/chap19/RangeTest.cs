98058c586723022a3225818f168a02db70e6505d
nakaji <nakaji126@gmail.com>
2013-09-19 00:16:11 +0900

    19.4 Refactoring

--- MSTestExerciseTest/chap19/RangeTest.cs
+++ MSTestExerciseTest/chap19/RangeTest.cs
@@ -8,52 +8,48 @@ namespace MSTestExerciseTest.chap19
     [TestClass]
     public class RangeTest
     {
+        private Range sut;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            sut = new Range(1.1d, 9.98d); 
+        }
+
         [TestMethod]
         public void オブジェクト生成()
         {
-            var range = new Range(1.1d, 9.98d);
-
-            Assert.AreEqual(1.1d, range.Min);
-            Assert.AreEqual(9.98d, range.Max);
+            Assert.AreEqual(1.1d, sut.Min);
+            Assert.AreEqual(9.98d, sut.Max);
         }
 
         [TestMethod]
         public void 最小値未満の場合False()
         {
-            var sut = new Range(1.1d, 9.98d);
-
             Assert.IsFalse(sut.Contains(1.099999d));
         }
 
         [TestMethod]
         public void 最小値の場合True()
         {
-            var sut = new Range(1.1d, 9.98d);
-
             Assert.IsTrue(sut.Contains(1.1d));
         }
 
         [TestMethod]
         public void 最大値の場合True()
         {
-            var sut = new Range(1.1d, 9.98d);
-
             Assert.IsTrue(sut.Contains(9.98d));
         }
 
         [TestMethod]
         public void 最大値より大きい場合False()
         {
-            var sut = new Range(1.1d, 9.98d);
-
             Assert.IsFalse(sut.Contains(9.980001d));
         }
 
         [TestMethod]
         public void 範囲内はTrue()
         {
-            var sut = new Range(1.1d, 9.98d);
-
             Assert.IsTrue(sut.Contains(5.555d));
         }
     }
