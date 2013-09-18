5ce82c8cddccd2ff5dc82c6cfa9ef4449e960ba1
nakaji <nakaji126@gmail.com>
2013-09-18 23:50:10 +0900

    19.4 Rangeクラスを追加

---
+++ MSTestExercise/chap19/Range.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSTestExercise.chap19
+{
+    public class Range
+    {
+        public double Min { get; private set; }
+        public double Max { get; private set; }
+
+        public Range(double min, double max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
---
+++ MSTestExerciseTest/chap19/RangeTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExercise;
+using MSTestExercise.chap19;
+
+namespace MSTestExerciseTest.chap19
+{
+    [TestClass]
+    public class RangeTest
+    {
+        [TestMethod]
+        public void オブジェクト生成()
+        {
+            var range = new Range(1.1d, 9.98d);
+
+            Assert.AreEqual(1.1d, range.Min);
+            Assert.AreEqual(9.98d, range.Max);
+        }
+    }
+}
