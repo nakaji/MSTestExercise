a1fab5294c6ce148c3d6a65c0cb5fc964018b837
nakaji <nakaji126@gmail.com>
2013-09-21 01:29:42 +0900

    19.5 ConsumptionTaxクラスを追加

---
+++ MSTestExercise/chap19/ConsumptionTax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSTestExercise.chap19
+{
+    public class ConsumptionTax
+    {
+        private int _taxRate;
+
+        public ConsumptionTax(int taxRate)
+        {
+            _taxRate = taxRate;
+        }
+    }
+}
---
+++ MSTestExerciseTest/chap19/ConsumptionTaxTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExercise;
+using MSTestExercise.chap19;
+
+namespace MSTestExerciseTest.chap19
+{
+    [TestClass]
+    public class ConsumptionTaxTest
+    {
+        [TestMethod]
+        public void オブジェクトの生成()
+        {
+            var sut = new ConsumptionTax(5);
+        }
+    }
+}
